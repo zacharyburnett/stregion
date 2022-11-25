e3c1c3f99ad69c5a1ca17c316eeaa4a6b79b65fb
Zach Burnett <zachary.r.burnett@gmail.com>
2022-11-25 15:24:48 -0500

    use PEP621 configuration (#10)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import warnings
-from setuptools import setup, Extension, find_packages
 
+from setuptools import setup, Extension
 
 # check if cython or pyrex is available.
 pyrex_impls = 'Cython.Distutils.build_ext', 'Pyrex.Distutils.build_ext'
@@ -21,74 +21,37 @@ else:
     cmdclass = {}
     PYREX_SOURCE = "src/_region_filter.c"
 
-
-# If you don't want to build filtering module (which requires a C
-# compiler), set it to False
+# If you don't want to build filtering module (which requires a C compiler), set it to False
 WITH_FILTER = True
 
-with open('stregion/version.py') as version_data:
-    for line in version_data:
-        if (line.startswith('__version__')):
-            version = ''.join(line.strip().split('"')[1::2])
+arguments = dict()
 
+if WITH_FILTER:
+    try:
+        import numpy
+    except ImportError:
+        warnings.warn(
+            "numpy must be installed to build the filtering module.")
+        sys.exit(1)
 
-def main():
-    install_requires = [
-        'numpy',
-        'pyparsing>=2.0.0'
+    try:
+        numpy_include = numpy.get_include()
+    except AttributeError:
+        numpy_include = numpy.get_numpy_include()
+
+    if len(cmdclass) > 0:
+        arguments["cmdclass"] = cmdclass
+
+    arguments["ext_modules"] = [
+        Extension(
+            "stregion._region_filter",
+            [PYREX_SOURCE],
+            include_dirs=[
+                './src',
+                numpy_include,
+            ],
+            libraries=[],
+        )
     ]
 
-    ka = dict(name="stregion",
-              version=version,
-              description="python parser for ds9 region files",
-              author="Jae-Joon Lee",
-              author_email="lee.j.joon@gmail.com",
-              url="http://github.com/spacetelescope/stregion/",
-              download_url="http://github.com/spacetelescope/stregion/releases",
-              license="MIT",
-              platforms=["Linux", "MacOS X"],
-              packages=find_packages('.'),
-              install_requires=install_requires,
-              use_2to3=False,
-              )
-    ka["classifiers"] = ['Development Status :: 5 - Production/Stable',
-                         'Intended Audience :: Science/Research',
-                         'License :: OSI Approved :: MIT License',
-                         'Operating System :: MacOS :: MacOS X',
-                         'Operating System :: POSIX :: Linux',
-                         'Programming Language :: Cython',
-                         'Programming Language :: Python',
-                         'Programming Language :: Python :: 3',
-                         'Topic :: Scientific/Engineering :: Astronomy',
-                         ]
-
-    if WITH_FILTER:
-        try:
-            import numpy
-        except ImportError:
-            warnings.warn(
-                "numpy must be installed to build the filtering module.")
-            sys.exit(1)
-
-        try:
-            numpy_include = numpy.get_include()
-        except AttributeError:
-            numpy_include = numpy.get_numpy_include()
-
-        if cmdclass:
-            ka["cmdclass"] = cmdclass
-
-        ka["ext_modules"] = [Extension("stregion._region_filter",
-                                       [PYREX_SOURCE],
-                                       include_dirs=['./src',
-                                                     numpy_include,
-                                                     ],
-                                       libraries=[],
-                                       )
-                             ]
-
-    setup(**ka)
-
-
-if __name__ == "__main__":
-    main()
+setup(**arguments)
